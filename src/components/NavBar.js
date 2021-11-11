5de50f7709224b3a34ae4e009cd06ec00ac83374
Raiannbr <braian.link@gmail.com>
2021-11-11 18:50:14 -0300

    Primer entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar () {
+    return <div>
+            <nav>
+                <ul>
+                    <li>Contacto</li>
+                    <li>Producto</li>
+                    <li>Carrito</li>
+                </ul>
+            </nav>
+        </div>
+}
+export default NavBar
