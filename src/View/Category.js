86d33bf5fce0e4430209cac57ed811778610fcc1
Raiannbr <67348873+Raiannbr@users.noreply.github.com>
2021-12-09 18:25:23 -0300

    Update Category.js

--- src/View/Category.js
+++ src/View/Category.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
-import ItemDetailContainer from "../components/ItemDetail/ItemDetailContainer"
+// import ItemDetailContainer from "../components/ItemDetail/ItemDetailContainer"
+import ItemListContainer from '../components/ItemListContainer/ItemListContainer'
+import { useParams } from 'react-router-dom'
 
 const Category = () => {
+
+    const params = useParams();
+
+    // params.id
+
+    console.log(params.plato)
     return (
         <div>
             <div className="row row-cols-sm-1" >
                 <div className="col d-flex">
-                <ItemDetailContainer
-            
+                <ItemListContainer
+                    categoryId = {params.plato}
+                    
+                
                 />
                 </div>
             </div>
